20af2f9efd5d859c4c760ab289b2d124f5c79928
alifahnurhana <alifahnur2609@gmail.com>
2021-09-10 14:10:17 +0800

    10/09/2021/view dan view group

--- app/src/main/java/com/example/myintentapp/MainActivity.kt
+++ app/src/main/java/com/example/myintentapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myintentapp
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -16,16 +17,21 @@ class MainActivity : AppCompatActivity() {
             val moveIntent = Intent(this, MoveActivity::class.java)
             startActivity(moveIntent)
         }
+
         val btnActivityData: Button = findViewById(R.id.btn_move_activity_data)
         btnActivityData.setOnClickListener {
-
             val moveWithDataIntent = Intent(this@MainActivity, MoveWithDataActivity::class.java)
             moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "DicodingAcademy Boy")
             moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 5)
             startActivity(moveWithDataIntent)
-
         }
 
+        val btnDialPhone: Button = findViewById(R.id.btn_dial_number)
+        btnDialPhone.setOnClickListener {
+            val phoneNumber = "08990814899"
+            val dialPhoneIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phoneNumber"))
+            startActivity(dialPhoneIntent)
+        }
 
     }
 
